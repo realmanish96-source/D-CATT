fd779c6f6a15cddb1645ec24a57a7294f5e12ac9
manish jha <realmanish96@gmail.com>
2026-09-03 14:00:18 +0530

    adding splash screen and logo for D-CATT

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -1,6 +1,7 @@
 import sys
-from PySide6.QtWidgets import QApplication,QPushButton,QLabel,QWidget,QVBoxLayout, QHBoxLayout,QMainWindow
+from PySide6.QtWidgets import QApplication,QPushButton,QLabel,QWidget,QVBoxLayout, QHBoxLayout,QMainWindow,QSplashScreen
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QPixmap,QIcon
 
 class MainTerminalWindow(QMainWindow):
     def __init__(self):
@@ -8,6 +9,8 @@ class MainTerminalWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
+
+
         self.setWindowTitle("D-CATT")
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
@@ -17,16 +20,38 @@ class MainTerminalWindow(QMainWindow):
         vertical_layout.addWidget(self.default_font)
 
         self.default_font.setAlignment(Qt.AlignTop)
-        
+
+        self.setStyleSheet("""
+                            QMainWindow{
+                            background-color: black;
+                                        }
+                            """)
+
 
         
 
 
 def Front_Window():
     terminal = QApplication(sys.argv)
+
+    #logo
+    terminal.setWindowIcon(QIcon("D-CATT\\app\\assets\\D-CATT logo.png"))
+    #splash screen
+    pixmap = QPixmap("D-CATT\\app\\assets\\D-CATT logo.png")
+    pixmap = pixmap.scaled(
+        200,
+        200,
+        Qt.KeepAspectRatio,
+        Qt.SmoothTransformation
+    )
+    splash = QSplashScreen(pixmap)
+    splash.show()
+
     window = MainTerminalWindow()
     window.showMaximized()
+    splash.finish(window)
     sys.exit(terminal.exec())
+
 
 if (__name__ == '__main__'):
     Front_Window()
